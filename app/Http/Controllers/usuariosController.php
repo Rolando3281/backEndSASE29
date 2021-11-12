c58fa6578a8393eefd514fcdd708d9ae8871336e
rolandomarroquin <rolando.marroquin@sacsa-ca.com>
2021-11-12 17:00:27 -0600

    add Jason Web Token TTL Custom

--- app/Http/Controllers/usuariosController.php
+++ app/Http/Controllers/usuariosController.php
@@ -147,7 +147,7 @@ class usuariosController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL()
         ]);
     }
 }
